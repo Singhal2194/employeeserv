b7539081bfcb367a1d41651d4fb8d51eee92a0dd
satish.agarwal <satish.agarwal@olacabs.com>
2021-02-25 10:22:32 +0530

    added getByEmpId

--- employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/dao/AddressDao.java
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/dao/AddressDao.java
@@ -4,6 +4,10 @@ import com.paypal.bfs.test.employeeserv.entity.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressDao extends JpaRepository<AddressEntity, Integer> {
+
+    Optional<AddressEntity> findByEmpId(Integer integer);
 }
--- employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/dao/EmployeeDao.java
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/dao/EmployeeDao.java
@@ -4,6 +4,10 @@ import com.paypal.bfs.test.employeeserv.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeDao extends JpaRepository<EmployeeEntity, Integer> {
+
+        Optional<EmployeeEntity> findByEmpId(Integer integer);
 }
