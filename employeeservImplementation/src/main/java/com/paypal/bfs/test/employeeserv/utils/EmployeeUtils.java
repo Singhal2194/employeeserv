c2259ab807c311054877757b2a8538fbe10d851d
satish.agarwal <satish.agarwal@olacabs.com>
2021-02-25 10:24:21 +0530

    added employee utils

---
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/utils/EmployeeUtils.java
@@ -0,0 +1,12 @@
+package com.paypal.bfs.test.employeeserv.utils;
+
+import java.util.Random;
+
+public class EmployeeUtils {
+
+    public static Integer generateUniqueEmpId() {
+
+        Random random = new Random();
+        return random.nextInt(10000);
+    }
+}
